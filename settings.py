3dcf214560105d278029ea24d7d7411807801d68
Yellowpig-zp <zipeng.qin.1997@outlook.com>
2018-06-17 16:29:04 -0700

    added settings.py - contains absolute path to different files

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Directory of the original imagenet images
+ORIGINAL_IMAGE_PATH = "/home/shared/rodia/datasets/imagenet/train_256x256/"
+
+# Directory of the edge maps
+EDGEMAP_PATH = "/home/qinzp97/rodia/hdd1/ImageNet_edge_maps/"
+
