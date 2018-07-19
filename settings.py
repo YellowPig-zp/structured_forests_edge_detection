c47c3480b9110883f89bf2f1ca4fa33dc24d725c
Yellowpig-zp <zipeng.qin.1997@outlook.com>
2018-07-19 12:59:41 -0700

    changed the directory to work with the resized imagenet samples

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 # Directory of the original imagenet images
-ORIGINAL_IMAGE_PATH = "/home/shared/rodia/datasets/imagenet/train_256x256/"
+ORIGINAL_IMAGE_PATH = "/home/shared/rodia/datasets/imagenet/train_64x64/"
 
 # Directory of the edge maps
-EDGEMAP_PATH = "/home/qinzp97/rodia/hdd1/ImageNet_edge_maps/"
+EDGEMAP_PATH = "/home/qinzp97/hoth/hdd1/ImageNet_edge_maps_resized/"
 
 # picked original images path
 PICKED_ORIGINALS_PATH = "/home/qinzp97/rodia/hdd1/picked_dataset/ImageNet_originals/"
